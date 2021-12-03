3a506167720cbcc7946becf5b9ff6c3ee1f52364
Olesya Zubkova <olesya_2802@mail.ru>
2021-12-03 14:51:11 +0300

    task updated

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,9 @@
 import GameSavingLoader from './loader';
-import json from './parser';
 
 (async () => {
-  const data = await GameSavingLoader.load();
   try {
-    const value = await json(data);
-    return value;
-  } catch {
-    // eslint-disable-next-line
-    return error;
+    return await GameSavingLoader.load();
+  } catch (error) {
+    return Promise.reject(error);
   }
 })();
